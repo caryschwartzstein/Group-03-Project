21f5e3046fe23cec7066153c107d060b2d73bb5f
Fionnuala Howell <fionnualahowell@berkeley.edu>
2016-12-08 01:27:16 -0800

    Fixed picture not showing up bug

--- ezgarden-app/screens/progress.js
+++ ezgarden-app/screens/progress.js
@@ -19,7 +19,8 @@ export function ProgressState(number, image, message, unlocked) {
       this.unlocked = "You have unlocked the " + unlocked + " seed.";
     }
 }
-let state0 = new ProgressState(0, assets.images.PlantPot0, "By successfully taking care of your plants - you can unlock new seeds to plant!", "");
+
+let state0 = new ProgressState(0, assets.images.plantPot0, "By successfully taking care of your plants - you can unlock new seeds to plant!", "");
 let state1 = new ProgressState(1, assets.images.PlantPot1, "You have planted your first plant! Go you!", "sunflower");
 let state2 = new ProgressState(2, assets.images.PlantPot2, "Congratulations! You have successfully watered your new plants for a week!", "tulip");
 let state3 = new ProgressState(3, assets.images.PlantPot3, "Congrats - your garden is full!", "mint");
@@ -27,27 +28,17 @@ let state4 = new ProgressState(4, assets.images.PlantPot4, "You have successfull
 let state5 = new ProgressState(5, assets.images.PlantPot5, "Your plants are looking great! You have been taking care of them without notifications.", "tomato");
 let state6 = new ProgressState(6, assets.images.PlantPot6, "Wow you have multiple gardens growing healthily - you are on your way to becoming a successful gardener!", "chrysanthemum");
 
-export var state = state0;
-
-function checkStateChange() {
-   if (plants.gardens[0].plants.length == 3) {
-       state = state3;
-   }
-   return new Picture({ active: true, left: 0, right: 0, top: 50, bottom: 30, url: state.image })
-
-}
+var state = state1;
 
 var ProgressScreen = Column.template($ => ({
-	top: 0, bottom: 0, left: 0, right: 0, active: true,
-    skin: whiteSkin,
+	top: 0, bottom: 0, left: 0, right: 0, skin: whiteSkin,
     contents: [
         new assets.Header({
 			string: "Progress",
-			leftElement: new assets.ImgButton({ url: assets.images.home2, callFunc: screenUtils.showHome }),
+			leftElement: new assets.ImgButton({ url: assets.images.home2, callFunc: screenUtils.showHome })
 		}),
 	    new Picture({ left: 0, right: 0, top: 30, bottom: 30, url: state.image }),
-	    //new checkStateChange(),
-	    new Line({ left: 10, right: 10, top: 0, height: 130,
+	    new Line({ left: 10, right: 10, top: 0, height: 90,
          contents:[
             new Text({ width: 200, left: 20, right: 20, top: 0, bottom: 50, style: blackText, string: state.message + " " + state.unlocked })
          ]
@@ -66,7 +57,6 @@ export function getScreen() {
 }
 
 export function refresh() {
-    //checkStateChange();
     if (plants.gardens[0].plants.length == 3) {
        state = state3;
     }
